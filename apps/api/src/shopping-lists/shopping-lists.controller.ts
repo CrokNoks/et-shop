8a44de6b949a0342e13ddd32592744b42006fd7f
Lucas GUERRIER <l.guerrier@kutta.fr>
2026-03-21 14:51:13 +0100

    fix(api): import Delete decorator in ShoppingListsController

--- apps/api/src/shopping-lists/shopping-lists.controller.ts
+++ apps/api/src/shopping-lists/shopping-lists.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Patch, Logger } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete, Logger } from '@nestjs/common';
 import { ShoppingListsService } from './shopping-lists.service';
 
 @Controller('shopping-lists')
