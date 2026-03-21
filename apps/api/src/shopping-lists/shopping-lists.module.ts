005485c23a9e34697df55e13fdd088d6f7ac9cec
Lucas GUERRIER <l.guerrier@kutta.fr>
2026-03-21 14:05:29 +0100

    fix(api): export and import SupabaseModule to resolve dependency error

--- apps/api/src/shopping-lists/shopping-lists.module.ts
+++ apps/api/src/shopping-lists/shopping-lists.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ShoppingListsController } from './shopping-lists.controller';
 import { ShoppingListsService } from './shopping-lists.service';
+import { SupabaseModule } from '../supabase/supabase.module';
 
 @Module({
+  imports: [SupabaseModule],
   controllers: [ShoppingListsController],
   providers: [ShoppingListsService]
 })
